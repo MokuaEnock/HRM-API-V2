e105d5966597c1cefa57ca1f9cfad20edcbcb191
Enock <enock.mokua@student.moringaschool.com>
2023-03-30 03:01:47 +0300

    generat eauth controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,23 @@
 class Api::V1::UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
+  def profile
+    render json: { user: current_user }, status: :accepted
+  end
+
   def create
     @user = User.create(user_params)
     if @user.valid?
-      render json: { user: UserSerializer.new(@user) }, status: :created
+      @token = encode_token({ user_id: @user.id })
+      render json: { user: @user, jwt: @token }, status: :created
     else
-      render json: { error: "Failed to create user" }, status: :unprocessable_entity
+      render json: { error: 'failed to create user' }, status: :unprocessable_entity
     end
   end
 
   private
 
   def user_params
-    # params.require(:user).permit(:username, :password)
     params.require(:user).permit(:username, :password, :password_confirmation)
   end
 end
