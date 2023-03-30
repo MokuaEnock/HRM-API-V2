124cb81c2bc301901d0e9d3ca4199528f09af17d
Enock <enock.mokua@student.moringaschool.com>
2023-03-30 03:49:17 +0300

    enable extension in postgresdatabase

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_29_225348) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_30_003635) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "uuid-ossp"
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
